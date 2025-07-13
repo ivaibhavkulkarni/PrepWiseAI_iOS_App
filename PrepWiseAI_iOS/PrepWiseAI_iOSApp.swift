710552d765f57f565c6f79806ea8041e675ed458
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-07-13 23:03:44 +0530

    Initial Commit

---
+++ PrepWiseAI_iOS/PrepWiseAI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepWiseAI_iOSApp.swift
+//  PrepWiseAI_iOS
+//
+//  Created by Vaibhav kulkarni on 13/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PrepWiseAI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
